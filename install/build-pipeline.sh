a8b39047c3583670c83c72dceddce3beda06d785
Staffan Olsson <staffan@repos.se>
2018-11-24 21:44:35 +0100

    Demonstrates a build-pipeline task without resources

---
+++ install/build-pipeline.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#set -e
+
+kubectl cluster-info
+read -p "Press enter to continue"
+
+kubectl label namespace default istio-injection=enabled
+
+kubectl create -f knative-releases/serving/previous/v0.2.2/istio-crds.yaml
+kubectl create -f knative-releases/serving/previous/v0.2.2/istio.yaml
+read -p "Press enter to continue"
+
+kubectl create -f knative-releases/serving/previous/v0.2.2/serving.yaml
+read -p "Press enter to continue"
+
+kubectl apply -f knative-releases/serving/previous/v0.2.2/monitoring-metrics-prometheus.yaml
+kubectl apply -f knative-releases/serving/previous/v0.2.2/monitoring-tracing-zipkin-in-mem.yaml
+read -p "Press enter to continue"
+
+kubectl apply -f $GOPATH/src/github.com/knative/build-pipeline
