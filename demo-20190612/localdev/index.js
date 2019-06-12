e13ccef610faa7c87bd1155d2d33a48fc186ee37
Staffan Olsson <staffan@repos.se>
2019-06-12 15:53:40 +0200

    Probably the most useful server ever

---
+++ demo-20190612/localdev/index.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+const listenHost = process.env.LISTEN_HOST || '127.0.0.1';
+const port = process.env.PORT || 8080;
+
+const GREETING = 'Hello';
+
+const server = http.createServer((req, res) => {
+  console.log('Got request', req.url, req.headers);
+  const reqstatus = /\W(\d{3})$/.exec(req.url);
+  const status = reqstatus ? parseInt(reqstatus[1]) : 200;
+  const reqdelay = /\Wdelay=(\d+)/.exec(req.url);
+  const delay = reqdelay ? parseInt(reqdelay[1]) : 100;
+  setTimeout(() => {
+    res.writeHead(status, {"Content-Type": "text/plain"});
+    res.end(`${GREETING} status=${status} after delay=${delay}ms\n`);
+  }, delay);
+});
+
+server.listen(port, listenHost, () => {
+  console.log(`Server running at http://${listenHost}:${port}/`);
+});
